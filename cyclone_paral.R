0a9442bb0417a2d28f0eac0a1b0528ec153ec000
E-Ping Rau <eprau@E-Pingde-MacBook-Air.local>
2018-02-23 14:57:24 +0100

    small modif concerning resolution, file path and accessory lines on the map

--- cyclone_paral.R
+++ cyclone_paral.R
@@ -25,8 +25,8 @@ RetrCycl = function(year, x0, x1, y0, y1, resol, thres = 150, mapout = T, mapnam
   cycl_filt = cycl[Season %in% year, ] #filter by year
   
   #set up global coordinates
-  lon = rep(seq(x0 + resol / 2, x1 - resol / 2, by = resol), (y1 - y0))
-  lat = rep(seq(y1 - resol / 2, y0 + resol / 2, by = -resol), each = (x1 - x0))
+  lon = rep(seq(x0 + resol / 2, x1 - resol / 2, by = resol), (y1 - y0) / resol)
+  lat = rep(seq(y1 - resol / 2, y0 + resol / 2, by = -resol), each = (x1 - x0) / resol)
   coord = SpatialPointsDataFrame(matrix(c(lon, lat), ncol = 2), data.frame(ID = seq(1:length(lon))),
                                  proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
   
@@ -52,7 +52,7 @@ RetrCycl = function(year, x0, x1, y0, y1, resol, thres = 150, mapout = T, mapnam
   values(ras.wind) = unlist(wind)
   values(ras.press) = unlist(press)
   
-  dirct = "/res_cycl/"
+  dirct = "res_cycl/"
   writeRaster(ras.freq, paste0(dirct, mapname, "_freq.grd"), format = "raster")
   writeRaster(ras.dur, paste0(dirct, mapname, "_dur.grd"), format = "raster")
   writeRaster(ras.wind, paste0(dirct, mapname, "_wind.grd"), format = "raster")
@@ -65,21 +65,27 @@ RetrCycl = function(year, x0, x1, y0, y1, resol, thres = 150, mapout = T, mapnam
     pdf(paste0(dirct, mapname, "_freq.pdf"))
     plot(ras.freq, col = rev(heat.colors(300)))
     map(database = "world", xlim = c(x0, x1), ylim = c(y0, y1), add = T)
-    abline(h = 24, v = 121, lty = 3)
+    abline(h = 0, v = 0) #equator and prime meridian
+    abline(h = c(-23.5, 23.5), lty = 2) #tropics
+    abline(h = 24, v = 121, lty = 3) #fushan
     dev.off()
     
     #plot duration
     pdf(paste0(dirct, mapname, "_dur.pdf"))
     plot(ras.dur, col = rev(heat.colors(300)))
     map(database = "world", xlim = c(x0, x1), ylim = c(y0, y1), add = T)
-    abline(h = 24, v = 121, lty = 3)
+    abline(h = 0, v = 0) #equator and prime meridian
+    abline(h = c(-23.5, 23.5), lty = 2) #tropics
+    abline(h = 24, v = 121, lty = 3) #fushan
     dev.off()
     
     #plot wind speed
     pdf(paste0(dirct, mapname, "_wind.pdf"))
     plot(ras.wind, col = rev(heat.colors(300)))
     map(database = "world", xlim = c(x0, x1), ylim = c(y0, y1), add = T)
-    abline(h = 24, v = 121, lty = 3)
+    abline(h = 0, v = 0) #equator and prime meridian
+    abline(h = c(-23.5, 23.5), lty = 2) #tropics
+    abline(h = 24, v = 121, lty = 3) #fushan
     dev.off()
     
     #plot atmospheric pressure
@@ -87,12 +93,14 @@ RetrCycl = function(year, x0, x1, y0, y1, resol, thres = 150, mapout = T, mapnam
     arg = list(at = seq(800, 1100, by = 50), labels = seq(800, 1100, by = 50))
     plot(ras.press, col = heat.colors(300), breaks = 800:1100, axis.args = arg, zlim = c(800, 1100))
     map(database = "world", xlim = c(x0, x1), ylim = c(y0, y1), add = T)
-    abline(h = 24, v = 121, lty = 3)
+    abline(h = 0, v = 0) #equator and prime meridian
+    abline(h = c(-23.5, 23.5), lty = 2) #tropics
+    abline(h = 24, v = 121, lty = 3) #fushan
     dev.off()
   }
 }
 
-#RetrCycl(year = 2001:2003, x0 = 0, x1 = 180, y0 = 0, y1 = 90, resol = 1, mapname = "NE")
-RetrCycl(year = 2001:2003, x0 = 0, x1 = 180, y0 = 0, y1 = 90, resol = 0.5, mapname = "fine_NE")
-RetrCycl(year = 2001:2003, x0 = -180, x1 = 180, y0 = -90, y1 = 90, resol = 1, mapname = "world")
-#RetrCycl(year = 2001:2003, x0 = -180, x1 = 180, y0 = -90, y1 = 90, resol = 0.5, mapname = "fine_world")
+#RetrCycl(year = 2001:2003, x0 = 0, x1 = 180, y0 = 0, y1 = 90, resol = 1, mapname = "NE_coarse")
+RetrCycl(year = 2001:2003, x0 = -180, x1 = 180, y0 = -90, y1 = 90, resol = 1, mapname = "world_coarse")
+RetrCycl(year = 2001:2003, x0 = 0, x1 = 180, y0 = 0, y1 = 90, resol = 0.5, mapname = "NE")
+RetrCycl(year = 2001:2003, x0 = -180, x1 = 180, y0 = -90, y1 = 90, resol = 0.5, mapname = "world")
